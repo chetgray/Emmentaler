d2f48f37fb4aa61474c3fd0b2ecb5956e101c9b6
Chet Gray <chetgray@gmail.com>
2019-07-26 11:30:21 -0400

    Annotate JSON properties in Player class

--- SwissTournament/Player.cs
+++ SwissTournament/Player.cs
@@ -1,11 +1,17 @@
 using System;
+using Newtonsoft.Json;
 
 namespace SwissTournament
 {
     public class Player : IPlayer
     {
+        [JsonProperty]
         public Guid Id { get; } = Guid.NewGuid();
+
+        [JsonProperty]
         public string Name { get; set; } = "";
+
+        [JsonProperty]
         public int Seed { get; set; } = 0;
 
         public override string ToString()
