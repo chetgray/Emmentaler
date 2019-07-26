78ad0c529ab6f73dc67f3311c8abab04a65441c7
Chet Gray <chetgray@gmail.com>
2019-07-26 11:30:21 -0400

    Basic Player constructors

--- SwissTournament/Player.cs
+++ SwissTournament/Player.cs
@@ -7,5 +7,16 @@ namespace SwissTournament
         public Guid Id { get; }
         public string Name { get; }
         public int Seed { get; }
+
+        public Player()
+        {
+            Id =  Guid.NewGuid();
+        }
+
+        public Player(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
     }
 }
