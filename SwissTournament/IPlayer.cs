741d4667499aefa471727cbcb95c318487942739
Chet Gray <chetgray@gmail.com>
2019-07-26 11:30:21 -0400

    Make Player.Name and Player.Seed writeable, refactor constructors
    Just use object initializer syntax to set Name.

--- SwissTournament/IPlayer.cs
+++ SwissTournament/IPlayer.cs
@@ -4,7 +4,7 @@ namespace SwissTournament
 {
     public interface IPlayer : IIdentifiable
     {
-        string Name { get; }
-        int Seed { get; }
+        string Name { get; set; }
+        int Seed { get; set; }
     }
 }
--- SwissTournament/Player.cs
+++ SwissTournament/Player.cs
@@ -4,20 +4,9 @@ namespace SwissTournament
 {
     public class Player : IPlayer
     {
-        public Guid Id { get; }
-        public string Name { get; } = "";
-        public int Seed { get; } = 0;
-
-        public Player()
-        {
-            Id =  Guid.NewGuid();
-        }
-
-        public Player(string name)
-        {
-            Id = Guid.NewGuid();
-            Name = name;
-        }
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; set; } = "";
+        public int Seed { get; set; } = 0;
 
         public override string ToString()
         {
